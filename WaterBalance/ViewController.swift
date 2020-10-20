c60fb63bedcb39aa0d4abf905893074de22ee221
Sergei Polivanov <devinice.net@gmail.com>
2020-10-20 15:33:54 +0300

    Initial Commit

---
+++ WaterBalance/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WaterBalance
+//
+//  Created by Sergei Polivanov on 20.10.2020.
+//  Copyright © 2020 Sergei Polivanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
